f1d7498401da4e38850a2374587adce03f87bc45
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2023-11-17 21:09:39 +0200

    Initial commit

---
+++ PostGirl/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PostGirl
+//
+//  Created by Haya Mikkawi on 17/11/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
